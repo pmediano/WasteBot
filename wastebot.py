a2bb26bc89d5efb1e4d73aa40214bea819bf386c
Pedro Martinez Mediano <pedro.martinez-mediano13@imperial.ac.uk>
2017-12-03 18:36:23 +0000

    Updated TO-DO list and added some inline comments.

--- wastebot.py
+++ wastebot.py
@@ -10,9 +10,12 @@ import base64
 """
 TODO list:
     - Implement persistent leaderboard
-    - Command to print current poll state
-    - Put token in separate encrypted file (to push to Github)
+    - Do not count other bots when calculating quorum
     - Ensassiment
+        * Randomised responses
+        * Stickerrrrrs
+        * Pretty leaderboard print
+
 """
 
 ENCODED_TOKEN  = 'LTB/7iAGE/OG4isvyJ7Nsr/zJ1kdqWqq2sEYqWPFJB6RF6PU6HURRqQc+oSa7lbF36ZyJSZi+/WrCAG9PQFIZw=='
@@ -30,7 +33,10 @@ cant_begin_phrases = ['Finish the previous story first',
                       'Shut up and let us listen to the previous story']
 
 def begin_callback(bot, update):
-    print 'begin'
+    """
+    Start telling a story and make the storyteller a wasteman candidate.
+    Does nothing if STATE != IDLE.
+    """
     global candidate, STATE
     group_id = update.message.chat_id
     if STATE != IDLE:
@@ -39,13 +45,14 @@ def begin_callback(bot, update):
         STATE = TELLING
         users.append(update.effective_user)
         candidate = update.effective_user
-        print 'candidate', candidate
-        print 'users', users
         sender_name = update.effective_user.first_name
         bot.send_message(chat_id=update.message.chat_id, text='Everyone shut up and listen to ' + sender_name)
 
+
 def end_callback(bot, update):
-    print 'end'
+    """
+    Finish a wasteman story and start poll.
+    """
     global STATE
     sender = update.effective_user.id
     group_id = update.message.chat_id
@@ -62,6 +69,7 @@ def end_callback(bot, update):
     bot.send_message(chat_id=group_id, text='Let the waste games begin!')
     STATE = POLLING
 
+
 def vote_callback(bot, update, vote):
     sender = update.effective_user.id
     sender_name = update.effective_user.first_name
@@ -74,29 +82,35 @@ def vote_callback(bot, update, vote):
         votes[sender] = vote
         check_result(bot, update)
 
+
 def leaderboard_callback(bot, update):
-    print 'leader'
+    """
+    Print current wasteman leaderboard.
+    """
     bot.send_message(chat_id=update.message.chat_id, text=str(leaderboard))
 
+
 def check_result(bot, update):
-    print 'check'
-    try:
-        waste_votes = len(filter(lambda x: x > 0, votes.values()))
-        nah_votes = len(filter(lambda x: x < 0, votes.values()))
-        # Do not count the bot or the poster himself
-        # TODO: would be better to not count any bots
-        group_id = update.message.chat_id
-        nb_group_members = bot.get_chat_members_count(group_id) - 2
-        quorum = int(nb_group_members/2) + 1
-        if waste_votes >= quorum:
-            finish_poll(bot, update, True)
-        if nah_votes >= quorum:
-            finish_poll(bot, update, False)
-    except:
-        print 'Something got fucked'
+    """
+    Check whether current poll reached quorum and can be ended.
+    """
+    waste_votes = len(filter(lambda x: x > 0, votes.values()))
+    nah_votes = len(filter(lambda x: x < 0, votes.values()))
+    # Do not count the bot or the poster himself
+    # TODO: would be better to not count any bots
+    group_id = update.message.chat_id
+    nb_group_members = bot.get_chat_members_count(group_id) - 2
+    quorum = int(nb_group_members/2) + 1
+    if waste_votes >= quorum:
+        finish_poll(bot, update, True)
+    if nah_votes >= quorum:
+        finish_poll(bot, update, False)
+
 
 def finish_poll(bot, update, is_waste):
-    print 'Finish poll'
+    """
+    End current poll and update leaderboard.
+    """
     global votes, STATE
     STATE = IDLE
     votes = {}
@@ -109,28 +123,29 @@ def finish_poll(bot, update, is_waste):
     else:
         bot.send_message(chat_id=update.message.chat_id, text='Not waste enough. Try again next time.')
 
+
 def votequery_callback(bot, update):
-    print 'query'
+    """
+    Print current state of poll and number of votes needed for quorum.
+    """
     group_id = update.message.chat_id
     if STATE != POLLING:
         bot.send_message(chat_id=group_id, text='Not polling')
     else:
-        print 'Print query'
         waste_votes = len(filter(lambda x: x > 0, votes.values()))
-        print 'waste_votes'
         nah_votes = len(filter(lambda x: x < 0, votes.values()))
-        print 'nah_votes'
         nb_group_members = bot.get_chat_members_count(group_id) - 2
-        print 'nb_group_members'
         quorum = int(nb_group_members/2) + 1
-        print 'quorum'
         s = 'Votes for waste: ' + str(waste_votes) + '\nVotes against: ' + str(nah_votes) + '\nVotes needed: ' + str(quorum)
-        print s
         bot.send_message(chat_id=group_id, text=s)
 
+
 def unknown(bot, update):
-    print 'unknown'
+    """
+    Reply to an unknown command.
+    """
     bot.send_message(chat_id=update.message.chat_id, text='Wtf u talking about?')
+
 
 def main():
 
@@ -142,6 +157,7 @@ def main():
 
     secret_key = args[1].rjust(16)
 
+    # Use AES decryption to obtain bot API
     cipher = AES.new(secret_key,AES.MODE_ECB)
     decoded = cipher.decrypt(base64.b64decode(ENCODED_TOKEN))
     token = decoded.strip()
